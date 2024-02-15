e28720aceece4960c63b95d16078b99f5f798ea0
Igor Figueiredo <igfigueiredo@outlook.com>
2024-02-15 12:33:52 +0100

    aantal aanpassan bij het product scherm

--- src/PlantenlustApp.java
+++ src/PlantenlustApp.java
@@ -1,13 +1,7 @@
-import model.Bestelling;
-import repository.BestellingRepository;
 import view.MainMenuView;
 
 public class PlantenlustApp {
     public static void main(String[] args) {
         MainMenuView.toonMainMenu();
-//        BestellingRepository repo = new BestellingRepository();
-//        Bestelling bestelling = repo.getBestelling(200);
-//        System.out.println("Sum: " + bestelling.getBedrag());
-//        bestelling.calcBedrag();
     }
 }
--- src/view/MainMenuView.java
+++ src/view/MainMenuView.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public final class MainMenuView {
 
     private static int stateMenu = 1;
-    private static int state = 1;
+    private static int stateChoice = 1;
 
     public static void toonMainMenu() {
         System.out.print("\n=======================================================");
@@ -16,24 +16,7 @@ public final class MainMenuView {
         System.out.print("\n=======================================================");
         System.out.println();
 
-        controleerKeuze(1);
-    }
-
-    private static void keuzeMenu(int keuze) {
-        System.out.print("\n=======================================================");
-
-        stateMenu = keuze;
-        switch (keuze) {
-            case 1 -> System.out.print("\nTyp 2 -> Bestellingen van een Leverancier.");
-            case 2 -> System.out.print("\nTyp 3 -> Bestelregels van een Bestelling.\nTyp 33 -> Bestellingsstatus aanpassen.");
-            case 3 -> System.out.print("\nTyp 4 -> Product te tonen.\nTyp 44 -> Aantal aanpassen.");
-            case 33 -> System.out.print("\nTyp 3 -> Bestelregels van een Bestelling. \nTyp 33 -> Bestellingsstatus aanpassen.");
-            case 4 -> System.out.print("\nTyp 0 -> Het programma afsluiten.");
-            case 44 -> System.out.print("\nTyp 4 -> Product te tonen.\nTyp 44 -> Het aantal aanpassen.");
-        }
-        if (keuze != 1) System.out.print("\nTyp 1 -> Terug naar Leverancierslijst.");
-        System.out.print("\n-> ");
-        controleerKeuze(getKeus());
+        controleerKeuze(stateChoice);
     }
 
     private static int getKeus() {
@@ -53,7 +36,7 @@ public final class MainMenuView {
         } catch (NumberFormatException e) {
             System.out.printf(e.getMessage());
         }
-        return state;
+        return stateChoice;
     }
 
     private static String getText() {
@@ -71,14 +54,14 @@ public final class MainMenuView {
             }
             case 2 -> {
                 System.out.print("\nTYP DE LEVERANCIER CODE (druk op enter): ");
-                state = getInt();
-                BestellingView.toonBestellingenVanLeverancier(state);
+                stateChoice = getInt();
+                BestellingView.toonBestellingenVanLeverancier(stateChoice);
                 keuzeMenu(keuze);
             }
             case 3 -> {
                 System.out.print("\nTYP DE BESTELNUMMER (druk op enter): ");
-                state = getInt();
-                BestelregelView.toonRegelsVanBestelling(state);
+                stateChoice = getInt();
+                BestelregelView.toonRegelsVanBestelling(stateChoice);
                 keuzeMenu(keuze);
             }
             case 33 -> {
@@ -88,7 +71,7 @@ public final class MainMenuView {
                 String status = getText();
                 BestellingRepository repo = new BestellingRepository();
                 repo.updateStatus(bestelNr, status);
-                BestellingView.toonBestellingenVanLeverancier(state);
+                BestellingView.toonBestellingenVanLeverancier(stateChoice);
                 keuzeMenu(keuze);
             }
             case 4 -> {
@@ -102,11 +85,30 @@ public final class MainMenuView {
                 System.out.print("\nPAS HET AANTAL AAN: ");
                 int aantal = getInt();
                 BestelregelRepository repo = new BestelregelRepository();
-                repo.updateAantal(state, artCode, aantal);
-                BestelregelView.toonRegelsVanBestelling(state);
+                repo.updateAantal(stateChoice, artCode, aantal);
+                BestelregelView.toonRegelsVanBestelling(stateChoice);
                 keuzeMenu(keuze);
             }
             default -> System.out.println("Ongeldige keuze. Kies een getal uit de lijst.");
         };
+    }
+
+    private static void keuzeMenu(int keuze) {
+        System.out.print("\n=======================================================");
+
+        if (keuze != 1) System.out.print("\nTyp 1 -> Terug naar Leverancierslijst.");
+        stateMenu = keuze;
+        switch (keuze) {
+            case 1 -> System.out.print("\nTyp 2 -> Bestellingen van een Leverancier.");
+            case 2 -> System.out.print("\nTyp 3 -> Bestelregels van een Bestelling.\nTyp 33 -> Bestellingsstatus aanpassen.");
+            case 3 -> System.out.print("\nTyp 4 -> Product te tonen.\nTyp 44 -> Aantal aanpassen.");
+            case 33 -> System.out.print("\nTyp 3 -> Bestelregels van een Bestelling. \nTyp 33 -> Bestellingsstatus aanpassen.");
+            case 4 -> System.out.print("\nTyp 44 -> Aantal aanpassen bij de bestelregel " + stateChoice);
+            case 44 -> System.out.print("\nTyp 4 -> Product tonen.\nTyp 44 -> Het aantal aanpassen.");
+        }
+        System.out.print("\nTyp 0 -> Het programma afsluiten.");
+
+        System.out.print("\n-> ");
+        controleerKeuze(getKeus());
     }
 }
